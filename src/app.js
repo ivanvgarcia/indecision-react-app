9a2ca307092779f6ef9618f023a8e5bffd8b2633
Ivan <ivanvlora@gmail.com>
2018-08-18 09:30:07 +0900

    added styles and components

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log('Indecision App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import './styles/style.scss';
+
+ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
